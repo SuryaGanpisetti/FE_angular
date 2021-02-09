fcc7e2c29ef83e4d1fe9540e82fcff296b6dbb18
suryateja <suryateja229@gmail.com>
2021-02-09 23:40:26 +0530

    angular commit

--- src/app/github-profile/github-profile.component.ts
+++ src/app/github-profile/github-profile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
 
 @Component({
   selector: 'app-github-profile',
@@ -7,9 +9,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GithubProfileComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
+    let id = this.route.snapshot.paramMap.get('id');
+    console.log(id);
+    /*this.route.paramMap.subscribe(params =>{
+      let id = +params.get('id');
+      console.log(id)
+    })*/
   }
 
 }
